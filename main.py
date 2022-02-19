10c9f0d7202e7ff30de8f067d1a5f8fed6869e62
looko <lookoutldz@outlook.com>
2022-02-19 17:18:32 +0800

    refine code

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import json
 import os.path
 import urllib.parse
 
 import requests
+from requests.exceptions import ProxyError, SSLError
 from retrying import retry
 from tqdm import tqdm
 
@@ -21,7 +18,7 @@ folder = 'bgm'
 
 # 深度优先下载文件
 # 1：如果 obj 是文件链接直接下载返回
-# 2：如果 url 是文件夹，则请求链接，拿到文件夹内的内容（obj list），for 循环迭代 dfs(obj)
+# 2：如果 obj 是文件夹，则请求链接，拿到文件夹内的内容（obj list），迭代 dfs(obj)
 def dfs(parent, obj):
     name = obj['name']
     path = folder + parent + name
@@ -38,23 +35,27 @@ def dfs(parent, obj):
         downloads(path, file_url, size, offset)
     else:
         mkdir(path)
-        data = getfiles(dir_url)
-        for sub in iter(data):
-            dfs(parent + name + '/', sub)
+        try:
+            data = getfiles(dir_url)
+            for sub in iter(data):
+                dfs(parent + name + '/', sub)
+        except ProxyError:
+            print('Network Error (PROXY) for url: ' + dir_url)
+        except SSLError:
+            print('Network Error (SSL) for url: ' + dir_url)
 
 
+# 获取文件目录
 @retry(stop_max_attempt_number=5, stop_max_delay=30)
 def getfiles(url):
     obj = json.loads(requests.post(url, headers).text)
-    if obj is not None:
-        if 'files' not in obj:
-            return []
-        else:
-            return obj['files']
+    if obj is not None and 'files' in obj:
+        return obj['files']
     else:
         return []
 
 
+# 是否已经下载了完整的文件
 def fully_downloaded(path, size):
     if size - os.path.getsize(path) == 0:
         print('fully downloaded: ' + path)
@@ -62,6 +63,7 @@ def fully_downloaded(path, size):
     return False
 
 
+# 断点续传偏移量
 def resume_offset(path, size):
     if not os.path.exists(path):
         return 0
@@ -71,35 +73,39 @@ def resume_offset(path, size):
         return size - os.path.getsize(path)
 
 
+# 下载内容
 def downloads(filename, url, size, offset):
-    data = requests.post(url, headers)
-    pbar = tqdm(total=size, initial=offset, unit='b', unit_scale=True, desc=filename)
-    if data.status_code == 206:
-        with open(filename, 'a') as f:
-            for chunk in data.iter_content(chunk_size=1024):
-                if chunk:
-                    offset += len(chunk)
-                    f.write(chunk)
-                    f.flush()
-                    pbar.update(len(chunk))
-                    pbar.refresh()
-    elif data.status_code == 200:
-        with open(filename, 'wb') as f:
-            for chunk in data.iter_content(chunk_size=1024):
-                if chunk:
-                    offset += len(chunk)
-                    f.write(chunk)
-                    f.flush()
-                    pbar.update(len(chunk))
-                    pbar.refresh()
+    response = requests.post(url, headers)
+    if response.status_code == 206:
+        # 服务器支持断点续传
+        write_to_file(filename, 'a', response, size, offset)
+    elif response.status_code == 200:
+        # 服务器不支持断点续传
+        write_to_file(filename, 'wb', response, size, offset)
     else:
-        print('statusCode[' + str(data.status_code) + '] for [' + filename + ']\n')
+        # 下载失败
+        print('statusCode[' + str(response.status_code) + '] for [' + filename + ']\n')
+
+
+# 写入文件
+def write_to_file(filename, level, response, size, offset):
+    # 进度条显示
+    pbar = tqdm(total=size, initial=offset, unit='b', unit_scale=True, desc=filename)
+    with open(filename, level) as f:
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:
+                offset += len(chunk)
+                f.write(chunk)
+                f.flush()
+                pbar.update(len(chunk))
+                pbar.refresh()
     pbar.close()
 
 
-def mkdir(dirname):
-    if dirname != '' and not os.path.exists(dirname):
-        os.mkdir(dirname)
+# 新建文件夹
+def mkdir(dir_name):
+    if dir_name != '' and not os.path.exists(dir_name):
+        os.mkdir(dir_name)
 
 
 # Press the green button in the gutter to run the script.
